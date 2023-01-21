3e4647a96c3e6b33819c5167b6c66f9a8dcaf22c
hgvalguna <hgvalguna@student.apc.edu.ph>
2023-01-21 12:43:01 +0800

    all

--- week5/assets/js/main.js
+++ week5/assets/js/main.js
@@ -1,3 +1,9 @@
+/**
+* Template Name: Personal - v4.10.0
+* Template URL: https://bootstrapmade.com/personal-free-resume-bootstrap-template/
+* Author: BootstrapMade.com
+* License: https://bootstrapmade.com/license/
+*/
 (function() {
   "use strict";
 
@@ -244,4 +250,3 @@
   new PureCounter();
 
 })()
-// Added js lesson #5
