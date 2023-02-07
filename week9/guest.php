60e2141d32dcd2be05b56a3aec6278d694c4917a
hgvalguna <hgvalguna@student.apc.edu.ph>
2023-02-07 14:32:25 +0800

    Update guest.php

--- week9/guest.php
+++ week9/guest.php
@@ -40,7 +40,7 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT id, firstname, lastname FROM MyGuests";
+$sql = "SELECT id, firstname, lastname FROM hgvalguna_myguest";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
